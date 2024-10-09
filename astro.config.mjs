d9eb6783c2d6f0d10219c203a2143e2011499361
sebastianterleira <seb.terleira1204@gmail.com>
2024-10-09 15:36:45 -0600

    feat: config site-map

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  image: {
+  integrations: [react(), sitemap()],
+  site: 'https://brillaire.vercel.app',
+image: {
     remotePatterns: [{ protocol: "https" }],
   }
 });
